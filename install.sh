157190d08ada135f9f347835ac9d62734dbeb3d8
hexis <hexis@Debian.example.com>
2023-04-04 03:07:30 -0400

    updated the way hexpmg can install.

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ NC='\033[0m' # No Color
 
 if (whiptail --title "install" --yesno "do you want to install hexpmg" 10 60) then
 	if command -v axel >/dev/null; then
-		chmod +x ~/hexpmg/hexpmg && sudo mv ~/hexpmg/hexpmg /bin && mkdir ~/.config/hexpmg/ && mv ~/hexpmg/update ~/.config/hexpmg/ && mv ~/hexpmg/uninstall ~/.config/hexpmg/ && sudo rm -r ~/hexpmg && echo -e "${green}Done"
+		git clone https://github.com/hexisXz/hexpmg.git && cd hexpmg && chmod +x ~/hexpmg/hexpmg && sudo mv ~/hexpmg/hexpmg /bin && mkdir ~/.config/hexpmg/ && mv ~/hexpmg/update ~/.config/hexpmg/ && mv ~/hexpmg/uninstall ~/.config/hexpmg/ && sudo rm -r ~/hexpmg && echo -e "${green}Done"
 
 	else
    	   if (whiptail --title "install" --yesno "ERROR: you NEED axel to run hexpmg would you like to install axel" 10 60) then
@@ -35,6 +35,6 @@ if (whiptail --title "install" --yesno "do you want to install hexpmg" 10 60) th
 	fi
 
 
-	chmod +x ~/hexpmg/hexpmg && sudo mv ~/hexpmg/hexpmg /bin && mkdir ~/.config/hexpmg/ && mv ~/hexpmg/update ~/.config/hexpmg/ && mv ~/hexpmg/uninstall ~/.config/hexpmg/ && sudo rm -r ~/hexpmg
+	git clone https://github.com/hexisXz/hexpmg.git && cd hexpmg && chmod +x ~/hexpmg/hexpmg && sudo mv ~/hexpmg/hexpmg /bin && mkdir ~/.config/hexpmg/ && mv ~/hexpmg/update ~/.config/hexpmg/ && mv ~/hexpmg/uninstall ~/.config/hexpmg/ && sudo rm -r ~/hexpmg
 	fi
 fi
