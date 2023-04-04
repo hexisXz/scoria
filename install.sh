f4aa54f45df1399b1ceb88825bdf4154f139a91a
hexis <hexis@Debian.example.com>
2023-04-04 12:10:02 -0400

    added curl support.

--- install.sh
+++ install.sh
@@ -16,20 +16,20 @@ if (whiptail --title "install" --yesno "do you want to install hexpmg" 10 60) th
 		git clone https://github.com/hexisXz/hexpmg.git && cd hexpmg && chmod +x ~/hexpmg/hexpmg && sudo mv ~/hexpmg/hexpmg /bin && mkdir ~/.config/hexpmg/ && mv ~/hexpmg/update ~/.config/hexpmg/ && mv ~/hexpmg/uninstall ~/.config/hexpmg/ && sudo rm -r ~/hexpmg && echo -e "${green}Done"
 
 	else
-   	   if (whiptail --title "install" --yesno "ERROR: you NEED axel to run hexpmg would you like to install axel" 10 60) then
+   	   if (whiptail --title "install" --yesno "ERROR: you NEED curl for hexpmg to work. do you want to install curl?" 10 60) then
    		if command -v pacman 2>/dev/null; then
-        		sudo pacman -S axel 
+        		sudo pacman -S curl
 
 	        fi
 
                    
 		if command -v apt 2>/dev/null; then
-			sudo apt install axel 
+			sudo apt install curl 
 
 	        fi
 
         	if command -v dnf 2>/dev/null; then
-			 sudo dnf install axel 
+			 sudo dnf install curl  
 
 	        fi
 	fi
