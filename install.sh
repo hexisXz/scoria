736fe1f052412deb0652f9b58c32b02f0ef3ad05
hexis <hexis@void.lan>
2023-06-02 00:08:24 -0400

    update

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ read e
 
 if [ "$e" = "y" ]; then
 	if command -v curl >/dev/null; then
-		cd ~/ && git clone https://github.com/hexisXz/scoria.git && cd ~/scoria && chmod +x ~/scoria/scoria && mkdir ~/.local/bin/scoria && mv ~/scoria/scoria ~/.local/bin/scoria && mkdir ~/.config/scoria/ && mv ~/scoria/update ~/.config/scoria/ && mv ~/scoria/uninstall ~/.config/scoria/ && sudo mv DOC/scoria.1 /usr/share/man/man8 && sudo rm -r ../scoria && echo -e "${green}Done" && echo 'make sure you put "export PATH="$HOME/.local/bin/scoria:$PATH" in your zshrc or you bashrc.'
+		cd ~/ && git clone https://github.com/hexisXz/scoria.git && cd ~/scoria && chmod +x ~/scoria/scoria && mkdir ~/.local/bin/scoria && mv ~/scoria/scoria ~/.local/bin/scoria && mkdir ~/.config/scoria/ && mv ~/scoria/update ~/.config/scoria/ && mv ~/scoria/uninstall ~/.config/scoria/ && sudo mv ~/scoria/DOC/scoria.1 /usr/share/man/man8 && sudo rm -r ~/scoria && echo -e "${green}Done" && echo 'make sure you put "export PATH="$HOME/.local/bin/scoria:$PATH" in your zshrc or you bashrc.'
 
 	else
    	   echo "ERROR: you need curl to use scoria. installing curl.."
