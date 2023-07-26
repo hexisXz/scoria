740e1e347b82ff22f63864e435acea2a5843d1fa
hexis <hexis@void.lan>
2023-07-26 17:13:48 -0400

    updated uninstall and install script.

--- install.sh
+++ install.sh
@@ -10,9 +10,26 @@ NC='\033[0m' # No Color
 
 
 
-echo -e "${blue}do you want to install scoria? (y/n)${blue}"
+if command -v pacman 2>/dev/null; then
+	sudo pacman -S wget curl && cd ~/ && git clone https://github.com/hexisXz/scoria.git && cd ~/scoria && sudo make
+fi
 
 
-cd ~/ && git clone https://github.com/hexisXz/scoria.git && cd ~/scoria && sudo make
+if command -v apt 2>/dev/null; then
+	 sudo apt install wget curl && cd ~/ && git clone https://github.com/hexisXz/scoria.git && cd ~/scoria && sudo make
+
+fi
+
+if command -v dnf 2>/dev/null; then
+	 sudo dnf install wget curl && cd ~/ && git clone https://github.com/hexisXz/scoria.git && cd ~/scoria && sudo make
+fi
+
+
+if command -v xbps-install 2>/dev/null; then
+	 sudo xbps-install -Sy wget curl && cd ~/ && git clone https://github.com/hexisXz/scoria.git && cd ~/scoria && sudo make
+fi
+
+
+
 
 
