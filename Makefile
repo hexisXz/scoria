8aed78f0f0e0c1e122791cc93ef36d426937ef33
hexis <hexis@void.lan>
2023-07-26 16:51:44 -0400

    scoria 3.0 update: scoria is now written in C

--- Makefile
+++ Makefile
@@ -1,7 +1,5 @@
 install: scoria.c scoria
-	gcc scoria.c -o scoria && sudo mv scoria /bin/ 
-	cd ~/ && git clone https://github.com/hexisXz/scoria.git && mkdir ~/.local/bin/scoria && mv ~/scoria/scoria ~/.local/bin/scoria && mkdir ~/.config/scoria/ && mv ~/scoria/update ~/.config/scoria/ && mv ~/scoria/uninstall ~/.config/scoria/ && sudo mv ~/scoria/DOC/scoria.1 /usr/share/man/man8 && sudo rm -r ~/scoria
+	 cd ~/ && git clone https://github.com/hexisXz/scoria.git && mkdir ~/.local/bin/scoria && mv ~/scoria/scoria ~/.local/bin/scoria && mkdir ~/.config/scoria/ && mv ~/scoria/update ~/.config/scoria/ && mv ~/scoria/uninstall ~/.config/scoria/ && sudo mv ~/scoria/DOC/scoria.1 /usr/share/man/man8 && gcc scoria.c -o scoria && sudo mv scoria /bin/ && sudo rm -r ~/scoria 
 
 uninstall:
-	rm ~/.local/bin/scoria/scoria
-	sudo rm -r ~/.local/bin/scoria && sudo rm -r ~/.config/scoria && echo "uninstalled." || echo "ERROR."
+	sudo rm /bin/scoria && sudo rm -r ~/.config/scoria && echo "uninstalled." || echo "ERROR."
--- install.sh
+++ install.sh
@@ -12,36 +12,4 @@ NC='\033[0m' # No Color
 
 printf "do you want to install scoria? (y/n) "
 
-read e
 
-
-if [ "$e" = "y" ]; then
-	if command -v curl >/dev/null; then
-		cd ~/ && git clone https://github.com/hexisXz/scoria.git && cd ~/scoria && chmod +x ~/scoria/scoria && mkdir ~/.local/bin/scoria && mv ~/scoria/scoria ~/.local/bin/scoria && mkdir ~/.config/scoria/ && mv ~/scoria/update ~/.config/scoria/ && mv ~/scoria/uninstall ~/.config/scoria/ && sudo mv ~/scoria/DOC/scoria.1 /usr/share/man/man8 && sudo rm -r ~/scoria && echo -e "${green}Done" && echo 'make sure you put "export PATH="$HOME/.local/bin/scoria:$PATH" in your zshrc or you bashrc.'
-
-	else
-   	   echo "ERROR: you need curl to use scoria. installing curl.."
-   	   if command -v pacman 2>/dev/null; then
-        	  sudo pacman -S curl
-
-	   fi
-
-                   
-	   if command -v apt 2>/dev/null; then
-		  sudo apt install curl 
-
-	   fi
-
-           if command -v dnf 2>/dev/null; then
-		   sudo dnf install curl  
-
-	   fi
-	fi
-
-
-
-if [ "$e" = "n" ]; then
-	exit 1
-fi
-
-fi
